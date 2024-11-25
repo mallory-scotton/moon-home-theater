fff5cbb5b56185f65b81a2a68015cab7b938072b
mallory-scotton <mallory.scotton@epitech.eu>
2024-11-25 19:29:23 +0100

    [ADD] Adding language types

---
+++ common/types/language.ts
@@ -0,0 +1,113 @@
+// Define a type for language
+export type Language =
+  | 'af'          // Afrikaans
+  | 'ar'          // Arabic
+  | 'bn'          // Bengali
+  | 'bs'          // Bosnian
+  | 'ca'          // Catalan
+  | 'cs'          // Czech
+  | 'da'          // Danish
+  | 'de'          // German
+  | 'el'          // Greek
+  | 'en'          // English
+  | 'es'          // Spanish
+  | 'et'          // Estonian
+  | 'fa'          // Persian
+  | 'fi'          // Finnish
+  | 'fr'          // French
+  | 'gu'          // Gujarati
+  | 'he'          // Hebrew
+  | 'hi'          // Hindi
+  | 'hr'          // Croatian
+  | 'hu'          // Hungarian
+  | 'id'          // Indonesian
+  | 'it'          // Italian
+  | 'ja'          // Japanese
+  | 'jw'          // Javanese
+  | 'kn'          // Kannada
+  | 'ko'          // Korean
+  | 'la'          // Latin
+  | 'lv'          // Latvian
+  | 'lt'          // Lithuanian
+  | 'mk'          // Macedonian
+  | 'ml'          // Malayalam
+  | 'mn'          // Mongolian
+  | 'mr'          // Marathi
+  | 'my'          // Burmese
+  | 'ne'          // Nepali
+  | 'nl'          // Dutch
+  | 'no'          // Norwegian
+  | 'pl'          // Polish
+  | 'pt'          // Portuguese
+  | 'pa'          // Punjabi
+  | 'ro'          // Romanian
+  | 'ru'          // Russian
+  | 'si'          // Sinhalese
+  | 'sk'          // Slovak
+  | 'sl'          // Slovenian
+  | 'so'          // Somali
+  | 'sq'          // Albanian
+  | 'sr'          // Serbian
+  | 'su'          // Sundanese
+  | 'sv'          // Swedish
+  | 'sw'          // Swahili
+  | 'ta'          // Tamil
+  | 'te'          // Telugu
+  | 'th'          // Thai
+  | 'tr'          // Turkish
+  | 'uk'          // Ukrainian
+  | 'ur'          // Urdu
+  | 'vi'          // Vietnamese
+  | 'cy'          // Welsh
+  | 'xh'          // Xhosa
+  | 'yi'          // Yiddish
+  | 'zu'          // Zulu
+  | 'tl'          // Tagalog
+  | 'is'          // Icelandic
+  | 'km'          // Khmer
+  | 'la'          // Latin
+  | 'ht'          // Haitian Creole
+  | 'sn'          // Shona
+  | 'tg'          // Tajik
+  | 'tk'          // Turkmen
+  | 'uz'          // Uzbek
+  | 'hy'          // Armenian
+  | 'mn'          // Mongolian
+  | 'ba'          // Bashkir
+  | 'be'          // Belarusian
+  | 'bs'          // Bosnian
+  | 'eo'          // Esperanto
+  | 'fo'          // Faroese
+  | 'ga'          // Irish
+  | 'gl'          // Galician
+  | 'jw'          // Javanese
+  | 'ku'          // Kurdish
+  | 'ky'          // Kyrgyz
+  | 'lb'          // Luxembourgish
+  | 'mg'          // Malagasy
+  | 'ml'          // Malayalam
+  | 'mt'          // Maltese
+  | 'ro'          // Romanian
+  | 'sc'          // Sardinian
+  | 'se'          // Northern Sami
+  | 'tl'          // Tagalog
+  | 'wa'          // Walloon
+  | 'xh'          // Xhosa
+  | 'zu';         // Zulu
+
+// Array of all the languages
+export const LANGUAGES: string[] = [
+  'af', 'ar', 'bn', 'bs', 'ca',
+  'cs', 'da', 'de', 'el', 'en',
+  'es', 'et', 'fa', 'fi', 'fr',
+  'gu', 'he', 'hi', 'hr', 'hu',
+  'id', 'it', 'ja', 'jw', 'kn',
+  'ko', 'la', 'lv', 'lt', 'mk',
+  'ml', 'mn', 'mr', 'my', 'ne',
+  'nl', 'no', 'pl', 'pt', 'pa',
+  'ro', 'ru', 'si', 'sk', 'sl',
+  'so', 'sq', 'sr', 'su', 'sv',
+  'sw', 'ta', 'te', 'th', 'tr',
+  'uk', 'ur', 'vi', 'cy', 'xh',
+  'yi', 'zu', 'tl', 'is', 'km'
+];

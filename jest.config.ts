540c41f7eb78946bc6a665b3a742630c0edd1d4b
mallory-scotton <mallory.scotton@epitech.eu>
2024-11-26 10:15:47 +0100

    [ADD] Adding jest configuration

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+// Dependencies
+import type { Config } from 'jest';
+
+// Configuration of jest
+const config: Config = {
+  verbose: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node'
+};
+
+// Export the configuration
+export default config;

82e29f6c16fad735b5f0ac22150a0ce7ff2d1f28
mallory-scotton <mallory.scotton@epitech.eu>
2024-11-26 11:03:24 +0100

    [ADD] Adding listen function to the server to wait after the synchronisation of the database

--- server/server.ts
+++ server/server.ts
@@ -17,7 +17,19 @@ app.use(compression());
 // Use routers
 app.use('/', mainRouter);
 
-// listen to the port
-app.listen(PORT, () => {
-  console.log(`HTTP Server is listening on, http://localhost:${PORT}`);
-});
+/**
+ * Start the HTTP server on the defined PORT
+ * @returns Nothing
+ */
+export const listen = (): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    try {
+      app.listen(PORT, () => {
+        console.log(`HTTP Server is listening on, http://localhost:${PORT}`);
+        resolve();
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+};

062495255d00ab7ad39c032919830a91f1cf4154
mallory-scotton <mallory.scotton@epitech.eu>
2024-11-28 18:09:48 +0100

    [ADD] Adding configuration for the MEDIA path

---
+++ server/config/media.ts
@@ -0,0 +1,17 @@
+// Dependencies
+import fs from 'fs';
+import path from 'path';
+import { APPDATA } from '../../common/constants/path';
+import { logger } from '../../common/utils/logger';
+
+// Media path
+export const MEDIA_PATH = path.join(APPDATA, 'Moon Home Theater', 'Media');
+
+// Verify media path
+try {
+  fs.mkdirSync(MEDIA_PATH, { recursive: true });
+  logger.debug('Media path checked!')
+} catch (error) {
+  logger.error('Error detected with the media path');
+  process.exit(1);
+}

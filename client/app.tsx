84646cbce9da380e9e500e9c37e335c9862b1103
mallory-scotton <mallory.scotton@epitech.eu>
2024-12-02 18:55:03 +0100

    [ADD] Adding base exemple of content block and card usage

--- client/app.tsx
+++ client/app.tsx
@@ -1,9 +1,25 @@
-import { StrictMode } from 'react';
+// Dependencies
+import { lazy, StrictMode } from 'react';
 import { render } from 'react-dom';
+
+// Styles
+import '@/sass/styles.scss';
+
+// Components
+import { Card } from '@/components/cards';
+import { ContentBlock } from '@/components/contentBlock';
 
 render(
   <StrictMode>
-    <h1>Hello World</h1>
+    <Card>
+      <ContentBlock
+        title="Title goes here"
+        subtitle="Secondary • text"
+        description="Description offers a more detailed explanation of the content, product, or feature being presented."
+        extra={{ label: 'Label', text: 'Secondary • text' }}
+        alignement="left"
+      />
+    </Card>
   </StrictMode>,
   document.getElementById('app')
 );

f0788eee753551e6a5fa1e837c017bb7182ebe7c
mallory-scotton <mallory.scotton@epitech.eu>
2024-12-02 20:52:10 +0100

    [ADD] Adding tray and an icon

--- client/index.ts
+++ client/index.ts
@@ -1,26 +1,56 @@
 /** Dependencies */
-import { BrowserWindow, app } from 'electron';
+import { BrowserWindow, app, Tray, Menu } from 'electron';
 import { productName } from '../package.json';
+import path from 'path';
+
+import icon from './assets/icons/favicon.ico';
 
 const isDebug = (): boolean => process.env.npm_lifecycle_event === 'start';
 
 declare const MOON_WEBPACK_ENTRY: string;
 declare const MOON_PRELOAD_WEBPACK_ENTRY: string;
 
-console.log('Application is running with', MOON_WEBPACK_ENTRY);
+// Setup the tray menu
+let tray: Tray | null = null;
 
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
+const createTray = () => {
+  tray = new Tray(path.join(__dirname, icon));
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Show App',
+      click: () => {}
+    },
+    {
+      type: 'separator'
+    },
+    {
+      label: 'Quit',
+      click: () => {}
+    }
+  ]);
+
+  tray.setToolTip('Moon Home Theater');
+  tray.setContextMenu(contextMenu);
+
+  tray.on('click', () => {});
+};
+
 const createWindow = (): void => {
+  createTray();
+
   const window = new BrowserWindow({
     title: `${productName} ${app.getVersion()}`,
     minWidth: 1080,
     minHeight: 720,
     webPreferences: {
       preload: MOON_PRELOAD_WEBPACK_ENTRY
-    }
+    },
+    icon: path.join(__dirname, icon)
   });
 
   if (isDebug()) {

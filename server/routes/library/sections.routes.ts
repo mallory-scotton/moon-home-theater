754a5e3274af27d32a7396df9d9e6758b24a5aec
mallory-scotton <mallory.scotton@epitech.eu>
2024-12-04 16:56:12 +0100

    [ADD] Linking sections controller with the sections router

--- server/routes/library/sections.routes.ts
+++ server/routes/library/sections.routes.ts
@@ -1,38 +1,41 @@
 // Dependencies
 import { Router } from 'express';
+import Schemas from '@/validators/library/sections.validator';
+import { validator } from '@/middlewares/validator.middleware';
+import Controller from '@/controllers/library/sections.controller';
 
 // Create a new express router
 const router = Router();
 
 // Get all libraries
-router.get('/');
+router.get('/', validator.query(Schemas.getAllSectionsSchema), Controller.getAllSections);
 
 // Create a library
-router.post('/');
+router.post('/', Controller.createSection);
 
 // Update a library
-router.put('/:sectionId');
+router.put('/:sectionId', Controller.updateSection);
 
 // Delete a library
-router.delete('/:sectionId');
+router.delete('/:sectionId', Controller.deleteSection);
 
 // Refresh the library
-router.get('/:sectionId/refresh');
+router.get('/:sectionId/refresh', Controller.refreshSection);
 
 // Search in the library
-router.get('/:sectionId/search');
+router.get('/:sectionId/search', Controller.searchInSection);
 
 // Get newest items
-router.get('/:sectionId/newest');
+router.get('/:sectionId/newest', Controller.getNewestItemsInSection);
 
 // Get recently added items
-router.get('/:sectionId/recentlyAdded');
+router.get('/:sectionId/recentlyAdded', Controller.getRecentlyAddedItemsInSection);
 
 // Get recently viewed items
-router.get('/:sectionId/recentlyViewed');
+router.get('/:sectionId/recentlyViewed', Controller.getRecentlyViewedInSection);
 
 // Get library items
-router.get('/:sectionId/:tag');
+router.get('/:sectionId/:tag', Controller.getItemsByTagInSection);
 
 // Export the router
 export default router;

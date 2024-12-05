27b303759f21ad99317122aa11f062e4edef618b
mallory-scotton <mallory.scotton@epitech.eu>
2024-12-05 13:47:34 +0100

    [ADD] Adding new constants for files extensions: video, audio, subtitle, image

--- common/constants/path.ts
+++ common/constants/path.ts
@@ -2,3 +2,9 @@
 export const APPDATA =
   process.env.LOCALAPPDATA ||
   (process.platform === 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + '/.local/share');
+
+// Export allowed file extensions
+export const VIDEO_EXTENSIONS = ['.mp4', '.avi', '.mkv'];
+export const AUDIO_EXTENSIONS = ['.mp3', '.ogg', '.wav'];
+export const SUBTITLE_EXTENSIONS = ['.srt'];
+export const IMAGE_EXTENSIONS = ['.png', '.gif', '.jpg', 'jpeg', 'svg'];

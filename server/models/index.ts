b05f1eb6eec305deccb8b1ca084071c73acfb163
mallory-scotton <mallory.scotton@epitech.eu>
2024-12-05 13:48:09 +0100

    [ADD] Adding models forwarding

---
+++ server/models/index.ts
@@ -0,0 +1,35 @@
+// Dependencies
+import { Account } from './account.model';
+import { Directory } from './directory.model';
+import { LibrarySection } from './librarySection.model';
+import { LibrarySectionPermission } from './librarySectionPermission.model';
+import { MediaItem } from './mediaItem.model';
+import './mediaItemSetting.model';
+import { MediaPart } from './mediaPart.model';
+import { MediaStream } from './mediaStream.model';
+import { MetadataItem } from './metadataItem.model';
+import './metadataItemSetting.model';
+import './metadataRelation.model';
+import { Preference } from './preference.model';
+import { SectionLocation } from './sectionLocation.model';
+import { StatisticsBandwidth } from './statisticsBandwidth.model';
+import './statisticsMedia.model';
+import { StatisticsResouces } from './statisticsResources.model';
+import './tag.model';
+import './tagging.model';
+
+// Forward access
+export {
+  Account,
+  Directory,
+  LibrarySection,
+  LibrarySectionPermission,
+  MediaItem,
+  MediaPart,
+  MediaStream,
+  MetadataItem,
+  Preference,
+  SectionLocation,
+  StatisticsBandwidth,
+  StatisticsResouces
+};

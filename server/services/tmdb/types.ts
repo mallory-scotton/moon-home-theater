4f8a42eefdfbe4d90e48b3f6c8aa1a43f67e8cef
mallory-scotton <mallory.scotton@epitech.eu>
2024-12-05 13:50:28 +0100

    [ADD] Adding new types for TMDB

--- server/services/tmdb/types.ts
+++ server/services/tmdb/types.ts
@@ -8,13 +8,40 @@ export type TmdbPosterSize = 'w92' | 'w154' | 'w342' | 'w500' | 'w780' | 'origin
 export type TmdbProfileSize = 'w45' | 'w185' | 'h632' | 'original';
 export type TmdbStillSize = 'w92' | 'w185' | 'w300' | 'original';
 
+// Interface for TMDB person
+export interface TmdbPerson {
+  adult?: boolean;
+  gender?: number;
+  id: number;
+  known_for_department?: string;
+  name?: string;
+  original_name?: string;
+  popularity?: number;
+  profile_path?: string;
+  cast_id?: number;
+  character?: string;
+  credit_id?: string;
+  order?: number;
+}
+
+// Interface for TMDB image
+export interface TmdbImage {
+  aspect_ratio: number;
+  height: number;
+  iso_639_1: string;
+  file_path: string;
+  vote_average: number;
+  vote_count: number;
+  width: number;
+}
+
 // Interface for the tmdb search options
 export interface TmdbSearchOptions {
   page?: number;
   year?: number;
   type?: 'movie' | 'tv' | 'keyword' | 'company' | 'collection' | 'person' | 'multi';
   language?: Language;
-  includeAdult?: boolean
+  includeAdult?: boolean;
 }
 
 // Interface for the tmdb search result
@@ -64,3 +91,174 @@ export interface TmdbSearchResult {
   total_pages: number;
   total_results: number;
 }
+
+// Interface for the tmdb details result
+export interface TmdbDetailsResult {
+  adult?: boolean;
+  backdrop_path?: string;
+  belongs_to_collection?: string;
+  budget?: number;
+  genres?: {
+    id: number;
+    name: string;
+  }[];
+  homepage?: string;
+  id: number;
+  imdb_id?: string;
+  origin_country?: string[];
+  original_language?: string;
+  original_title?: string;
+  overview?: string;
+  popularity?: number;
+  poster_path?: string;
+  production_companies: {
+    id: number;
+    logo_path?: string;
+    name?: string;
+    origin_country?: string;
+  }[];
+  production_countries: {
+    english_name?: string;
+    iso_639_1?: string;
+    name?: string;
+  }[];
+  release_date?: string;
+  revenue?: number;
+  runtime?: number;
+  spoken_languages?: {
+    english_name?: string;
+    iso_639_1?: string;
+    name?: string;
+  }[];
+  status?: string;
+  tagline?: string;
+  title?: string;
+  video?: boolean;
+  vote_average?: number;
+  vote_count?: number;
+  credits?: {
+    cast?: TmdbPerson[];
+    crew?: TmdbPerson[];
+  };
+  releases?: {
+    countries?: {
+      certification?: string;
+      descriptors?: string[];
+      iso_3166_1?: string;
+      primary?: boolean;
+      release_date?: string;
+    }[];
+  };
+  keywords?: {
+    keywords?: {
+      id: number;
+      name: string;
+    }[];
+  };
+  external_ids?: {
+    imdb_id?: string;
+    wikidata_id?: string;
+    facebook_id?: string;
+    instagram_id?: string;
+    twitter_id?: string;
+  };
+  release_dates?: {
+    results?: {
+      iso_3166_1?: string,
+      release_dates?: {
+        certification?: string;
+        descriptors?: string[];
+        iso_639_1?: string;
+        note?: string;
+        release_date?: string;
+        type?: number;
+      }[]
+    }[];
+  };
+  videos?: {
+    results?: {
+      iso_639_1?: string;
+      iso_3166_1?: string;
+      name?: string;
+      key?: string;
+      site?: string;
+      size?: number;
+      type?: string;
+      official?: boolean;
+      published_at?: string;
+      id?: string;
+    }[];
+  };
+  alternative_titles?: {
+    titles?: {
+      iso_3166_1?: string;
+      title?: string;
+      type?: string;
+    }[];
+  };
+  recommendations?: TmdbSearchResult;
+  review?: {
+    page: number;
+    results: {
+      author?: string;
+      author_details?: {
+        name?: string;
+        username?: string;
+        avatar_path?: string;
+        rating?: number;
+      };
+      content?: string;
+      created_at?: string;
+      id?: string;
+      updated_at?: string;
+      url?: string;
+    }[];
+    total_pages: number;
+    total_results: number;
+  };
+  similar?: TmdbSearchResult;
+  translations?: {
+    translations: {
+      iso_3166_1?: string;
+      iso_639_1?: string;
+      name?: string;
+      english_name?: string;
+      data?: {
+        homepage?: string;
+        overview?: string;
+        runtime?: number;
+        tagline?: string;
+        title?: string;
+      };
+    }[];
+  };
+  images?: {
+    backdrops?: TmdbImage[];
+    logos?: TmdbImage[];
+    posters?: TmdbImage[];
+  }
+}
+
+// Interface for the tmdb details options
+export interface TmdbDetailsOptions {
+  appendToResponse?: (
+    | 'credits'
+    | 'releases'
+    | 'keywords'
+    | 'external_ids'
+    | 'release_dates'
+    | 'videos'
+    | 'alternative_titles'
+    | 'recommendations'
+    | 'reviews'
+    | 'similar'
+    | 'translations'
+    | 'releases'
+    | 'images'
+    | 'content_ratings'
+  )[];
+  language?: Language;
+}
+
+// Type for details
+export type TmdbDetailsType = 'movie' | 'tv' | 'person' | 'network' | 'keyword' | 'company' | 'collection' | 'credit';

160744a1f4a90203bd120c87e07b9bc7927eaebd
mallory-scotton <mallory.scotton@epitech.eu>
2024-12-05 15:46:48 +0100

    [ADD] Adding tasks forwarding

---
+++ server/services/scanner/tasks/index.ts
@@ -0,0 +1,11 @@
+// Dependencies
+import { createMedia } from './media';
+import { getMetadataItem } from './metadata';
+import { updateCheck } from './updateCheck';
+
+// Forward dependencies
+export {
+  createMedia,
+  getMetadataItem,
+  updateCheck
+};
